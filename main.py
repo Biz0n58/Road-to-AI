324131674b4be69b08705a7e234ba05d34ebfa2a
f58dev <192527421+f58dev@users.noreply.github.com>
2026-03-12 08:10:52 +0300

    Variables

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+#String "I love Python programming!"            
+#Integer 42
+#Float 3.14 
+#Boolean True or False
+
+name = "Mercer"
+age = 22
+score = 97.5
+engineer = True
+print("My name is " + name + ", I am " + (age) + " years old, my score is " + str(score) + ", and it is " + str(engineer) + " that I am an engineer.")
+#Str 
+
+
+# Variables and User Input
 first_name = (input("Enter your First Name: "))
 last_name = (input("Enter your Last Name: "))
 print("Hello " + first_name + " " + last_name + "! Welcome to Python programming.")
+
