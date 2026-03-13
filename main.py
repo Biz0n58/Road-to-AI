09af52d61eda2bc3aa00e89dc7172821b6fbdb83
f58dev <192527421+f58dev@users.noreply.github.com>
2026-03-13 04:43:50 +0300

    Escape Char

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print("PyCharm")
-
 #String "I love Python programming!"
 #Integer 42
-#Float 3.14 
+#Float 3.14
 #Boolean True or False
 
 name = "Mercer"
@@ -10,8 +8,9 @@ age = 22
 score = 97.5
 engineer = True
 print("My name is " + name + ", I am " + str(age) + " years old, my score is " + str(score) + ", and it is " + str(engineer) + " that I am an engineer.")
-#-----------------------------------------------------------------------------------------------------------------------------------------# 
+#-----------------------------------------------------------------------------------------------------------------------------------------#
 #Arithmetic operators
+#parentheses (()) means "do this first"
 #exponents (**) means "to the power of"
 #multiplication (*)
 #division (/)
@@ -19,11 +18,12 @@ print("My name is " + name + ", I am " + str(age) + " years old, my score is " +
 #subtraction (-)
 
 #floor division (//) means "delete the after the decimal point"
-print("Hello World")
-print(5 * 2 + 4 / 2 - 3)
+
+print(5 * (2 + 4) / 2 - 3)
+
 num1 = 10
 num2 = 5
-sum = num1 + num2  
+sum = num1 + num2
 subtraction = num1 - num2
 multiplication = num1 * num2
 division = num1 / num2
@@ -34,10 +34,19 @@ print(division)
 
 
 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------#
+#Escape Characters
+
+
+print("Mohamed Said: \"I Like Football\"") # \' Single Quote
+print("ali\n love me") #\n	New Line
+print("Hello \t world") #\t	Tab
+print("Hello \b world") #\b	Backspace
+print("Hello \f world") #\f	Form Feed
 #-----------------------------------------------------------------------------------------------------------------------------------------#
 
 #Variables and User Input
 first_name = (input("Enter your First Name: "))
 last_name = (input("Enter your Last Name: "))
 print("Hello " + first_name + " " + last_name + "! Welcome to Python programming.")
-
